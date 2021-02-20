d3becf0372ba3469addaca1bef6e0d7c1ba72393
Ryan <ryan@ryanhalter.com>
2021-02-20 15:53:33 -0500

    Assignment 2

---
+++ Assignment 2/Assignment2Part2H.sql
@@ -0,0 +1,6 @@
+-- ***************************************************************************************************************************************************
+-- IT2351 Assignment 2 Part 2h - Ryan Halter
+-- ***************************************************************************************************************************************************
+
+INSERT INTO customers (customer_id, customer_first_name, customer_last_name, customer_address, customer_city, customer_state, customer_zip, customer_phone)
+VALUES (1738, "Thomas", "Buckingham", "123 Real St", "Cool City", 'TX', "98765", "1-234-567-8910")
